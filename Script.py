d65283ba2bb3e75fda3eec324d4ae0adab653f1a
Amrit Raj <98879654+Amritraj2008@users.noreply.github.com>
2022-06-18 17:56:07 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,7 +11,7 @@ class script(object):
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     REQUEST_TXT = """<b>NOTE:</b>
-- This bot is not a open source project. 
+
 - To Request a movie - https://t.me/req1bot  
 
 <b>DEVS:</b>
