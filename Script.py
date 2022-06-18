bcf091f8d5b87181730971cb464ebf9e913cf848
Amrit Raj <98879654+Amritraj2008@users.noreply.github.com>
2022-06-18 17:49:15 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -10,9 +10,9 @@ class script(object):
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-    SOURCE_TXT = """<b>NOTE:</b>
+    REQUEST_TXT = """<b>NOTE:</b>
 - This bot is not a open source project. 
-- Source - https://t.me/moviescartoons  
+- Request - https://t.me/req1bot  
 
 <b>DEVS:</b>
 - <a href=https://t.me/moviescartoons>Movies</a>"""
