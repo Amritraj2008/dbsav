494015d784baea0d85ac3725152ee6622cfcc0ff
Amrit Raj <98879654+Amritraj2008@users.noreply.github.com>
2023-06-02 11:34:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Amritraj:AaSs$def1234@cluster0.ajcgq.mongodb.net/Amrit?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Samriddhi:AaSs$def123@gjhff.vvelu.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "dbsav")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
